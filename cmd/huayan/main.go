29542f3fb68721fe557f25e190eb3d09b11b8db8
577732344@qq.com <577732344@qq.com>
2026-08-05 00:30:09 +0800

    发布华言 v0.4.0

--- cmd/huayan/main.go
+++ cmd/huayan/main.go
@@ -11,7 +11,7 @@ import (
 	"huayan/internal/formatter"
 )
 
-const version = "0.3.0"
+const version = "0.4.0"
 
 func main() {
 	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
